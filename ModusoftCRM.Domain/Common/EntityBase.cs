b137d8462a5f5ed45144d16a1cfadf31558430dd
Sila Kuscu <silaksc27@gmail.com>
2024-11-18 15:51:04 +0300

    [Update] required keyword added

--- ModusoftCRM.Domain/Common/EntityBase.cs
+++ ModusoftCRM.Domain/Common/EntityBase.cs
@@ -2,9 +2,9 @@
 {
     public abstract class EntityBase<TKey> : IEntityBase<TKey>, ICreatedByEntity, IModifiedByEntity, IDeletedByEntity
     {
-        public virtual TKey Id { get; set; }
+        public virtual required TKey Id { get; set; }
 
-        public virtual string CreatedByUserId { get; set; }
+        public virtual required string CreatedByUserId { get; set; }
         public virtual DateTimeOffset CreatedOn { get; set; }
         public virtual string? ModifiedByUserId { get; set; }
         public virtual DateTimeOffset? LastModifiedOn { get; set; }
