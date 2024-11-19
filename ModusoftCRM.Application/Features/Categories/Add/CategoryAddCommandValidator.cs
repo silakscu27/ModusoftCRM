1834d0e209fcffb54e1244506225acaacbcc9e5b
Sila Kuscu <silaksc27@gmail.com>
2024-11-19 14:14:07 +0300

    [Create] Created category add command validator

---
+++ ModusoftCRM.Application/Features/Categories/Add/CategoryAddCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace ModusoftCRM.Application.Features.Categories.Commands.Add
+{
+    public class CategoryAddCommandValidator : AbstractValidator<CategoryAddCommand>
+    {
+        public CategoryAddCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Ad alanı boş geçilemez.");
+
+            RuleFor(x => x.Name)
+                .MaximumLength(50)
+                .WithMessage("Ad alanı 50 karakterden büyük olamaz.");
+        }
+    }
+}
