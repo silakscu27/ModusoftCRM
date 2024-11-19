cb0b46df086c888e42472e712813245f233581bd
Sila Kuscu <silaksc27@gmail.com>
2024-11-19 16:16:44 +0300

    [Create] Created GetById folder's files

---
+++ ModusoftCRM.Application/Features/Categories/Queries/GetById/CategoryGetByIdQueryValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using ModusoftCRM.Application.Common.Interfaces;
+
+namespace ModusoftCRM.Application.Features.Categories.Queries.GetById
+{
+    public class CategoryGetByIdQueryValidator : AbstractValidator<CategoryGetByIdQuery>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CategoryGetByIdQueryValidator(ITenantService tenantService)
+        {
+            _context = tenantService.GetDbInstance(CancellationToken.None).GetAwaiter().GetResult();
+
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Lütfen bir kategori seçiniz.");
+
+            RuleFor(x => x.Id).MustAsync(AnyCategoryAsync).WithMessage("Seçili kategori bulunamadı.");
+
+        }
+
+        private Task<bool> AnyCategoryAsync(int id, CancellationToken cancellationToken)
+        {
+            return _context.Categories.AnyAsync(x => x.Id == id, cancellationToken);
+        }
+
+    }
+}
