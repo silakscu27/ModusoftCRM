4b76470193a80551f0629df2444b4407989b65c9
Sila Kuscu <silaksc27@gmail.com>
2024-11-19 16:02:37 +0300

    [Create] Created GetAll queries

---
+++ ModusoftCRM.Application/Features/Categories/Queries/GetAll/CategoryGetAllDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ModusoftCRM.Application.Features.Categories.Queries.GetAll
+{
+    public class CategoryGetAllDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty; // non nullable error
+        public string? Description { get; set; }
+    }
+}
