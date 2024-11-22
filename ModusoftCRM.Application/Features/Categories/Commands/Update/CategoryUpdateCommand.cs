72086e2ff6e898c94b5dfdb99e12c0ede50352e2
Sila Kuscu <silaksc27@gmail.com>
2024-11-22 15:57:54 +0300

    [Update] update category commands added

--- ModusoftCRM.Application/Features/Categories/Commands/Update/CategoryUpdateCommand.cs
+++ ModusoftCRM.Application/Features/Categories/Commands/Update/CategoryUpdateCommand.cs
@@ -5,6 +5,7 @@ namespace ModusoftCRM.Application.Features.Categories.Commands.Update
 {
     public class CategoryUpdateCommand : IRequest<Response<int>>
     {
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
     }
