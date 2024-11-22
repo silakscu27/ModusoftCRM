d88c956d3e1591e541bf4b2ca5ed57e208dbfd9c
Sila Kuscu <silaksc27@gmail.com>
2024-11-22 14:19:09 +0300

    [Create] Created Update Category commands

---
+++ ModusoftCRM.Application/Features/Categories/Commands/Update/CategoryUpdateCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Application.Features.Categories.Commands.Update
+{
+    public class CategoryUpdateCommand : IRequest<Response<int>>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
