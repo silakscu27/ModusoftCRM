2bb86e9cdc99e07c02874468812e9f1e3738d70c
Sila Kuscu <silaksc27@gmail.com>
2024-11-25 14:58:14 +0300

    [Create] Created CustomerConfiguration

---
+++ ModusoftCRM.Infrastructure/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,72 @@
+﻿using Finbuckle.MultiTenant;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ModusoftCRM.Domain.Entities;
+
+namespace ModusoftCRM.Infrastructure.Persistence.Configurations.Application
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+
+            // Id
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+            // Name
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.Name).HasMaxLength(200);
+
+            // Description
+            builder.Property(x => x.Description).IsRequired(false);
+            builder.Property(x => x.Description).HasMaxLength(2000);
+
+            // Picture
+            builder.Property(x => x.Picture).IsRequired(false);
+            builder.Property(x => x.Picture).HasMaxLength(500);
+
+            // ErpId
+            builder.Property(x => x.ErpId).IsRequired(false);
+            builder.Property(x => x.ErpId).HasMaxLength(150);
+
+            // Common Fields
+
+            // CreatedByUserId
+            builder.Property(x => x.CreatedByUserId).IsRequired();
+            builder.Property(x => x.CreatedByUserId).HasMaxLength(75);
+
+            // CreatedOn
+            builder.Property(x => x.CreatedOn).IsRequired();
+
+            // ModifiedByUserId
+            builder.Property(x => x.ModifiedByUserId).IsRequired(false);
+            builder.Property(x => x.ModifiedByUserId).HasMaxLength(75);
+
+            // LastModifiedOn
+            builder.Property(x => x.LastModifiedOn).IsRequired(false);
+
+            // DeletedByUserId
+            builder.Property(x => x.DeletedByUserId).IsRequired(false);
+            builder.Property(x => x.DeletedByUserId).HasMaxLength(75);
+
+            // DeletedOn
+            builder.Property(x => x.DeletedOn).IsRequired(false);
+
+            // IsDeleted
+            builder.Property(x => x.IsDeleted).IsRequired();
+            builder.Property(x => x.IsDeleted).HasDefaultValueSql("0");
+
+            // Relationships
+            //builder.HasMany<Franchise>(x => x.Franchises)
+              //  .WithOne(x => x.Customer)
+                //.HasForeignKey(x => x.CustomerId)
+                //.OnDelete(DeleteBehavior.Cascade);
+
+            builder.IsMultiTenant();
+
+            builder.ToTable("Customers");
+
+        }
+    }
+}
