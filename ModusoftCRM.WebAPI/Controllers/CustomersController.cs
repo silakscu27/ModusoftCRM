0cde39db34e6046f0a92f927503a3579ffedef27
Sila Kuscu <silaksc27@gmail.com>
2024-11-25 15:33:59 +0300

    [Create] Created Customers Controller

---
+++ ModusoftCRM.WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿using CRM.Application.Customers.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ModusoftCRM.WebAPI.Abstractions;
+
+namespace ModusoftCRM.WebApi.Controllers
+{
+    public sealed class CustomersController : ApiController
+    {
+        public CustomersController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAsync(CustomerAddCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+
+     
+    }
+}
