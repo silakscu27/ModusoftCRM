c01d2987a7444d1c69100f75aa3a72d7160496cd
Sila Kuscu <silaksc27@gmail.com>
2024-11-25 15:33:30 +0300

    [Create] Created Customer Add command

---
+++ ModusoftCRM.Application/Features/Customers/Commands/Add/CustomerAddCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using ModusoftCRM.Domain.Common;
+
+
+namespace CRM.Application.Customers.Commands
+{
+    public class CustomerAddCommand : IRequest<Response<Guid>>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? ErpId { get; set; }
+        public string? Picture { get; set; }
+        public string? PictureName { get; set; }
+        public List<Guid> CustomerTypeIds { get; set; } = new();
+        public bool AddFranchise { get; set; } = true;
+    }
+}
---
+++ ModusoftCRM.Application/Features/Customers/Commands/Add/CustomerAddCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using MediatR;
+using ModusoftCRM.Application.Common.Interfaces;
+using ModusoftCRM.Domain.Common;
+using CRM.Application.Customers.Commands;
+using ModusoftCRM.Domain.Entities;
+
+namespace CRM.Application.Features.Customers.Commands.Add
+{
+    public class CustomerAddCommandHandler : IRequestHandler<CustomerAddCommand, Response<Guid>>
+    {
+        private readonly ITenantService _tenantService;
+        private readonly IMapper _mapper;
+        private readonly ICurrentUserService _currentUserService;
+
+        public CustomerAddCommandHandler(ITenantService tenantService, IMapper mapper, ICurrentUserService currentUserService)
+        {
+            _tenantService = tenantService;
+            _mapper = mapper;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<Response<Guid>> Handle(CustomerAddCommand request, CancellationToken cancellationToken)
+        {
+            var dbContext = await _tenantService.GetDbInstance(cancellationToken);
+
+            var customer = _mapper.Map<Customer>(request);
+
+            customer.CreatedByUserId = _currentUserService.UserId
+                ?? throw new ArgumentNullException(nameof(_currentUserService.UserId)); // Kullanıcı ID kontrolü
+
+            await dbContext.Customers.AddAsync(customer, cancellationToken);
+
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return new Response<Guid>(customer.Id, "Müşteri başarıyla eklenmiştir.");
+        }
+    }
+}
