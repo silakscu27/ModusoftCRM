55c67eae89cca14b1538f3c724708b4bb6b011f0
Sila Kuscu <silaksc27@gmail.com>
2024-11-25 15:03:03 +0300

    [Create] Created CustomerRepository

---
+++ ModusoftCRM.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using ModusoftCRM.Domain.Entities;
+using ModusoftCRM.Domain.Repositories;
+using ModusoftCRM.Infrastructure.Context;
+using GenericRepository;
+
+namespace ModusoftCRM.Infrastructure.Repositories;
+internal sealed class CustomerRepository : Repository<Customer, ApplicationDbContext>, ICustomerRepository
+{
+    public CustomerRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
