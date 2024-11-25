290d498c2c1453a7fb6d3a2d6481a8c1c526d604
Sila Kuscu <silaksc27@gmail.com>
2024-11-25 14:48:56 +0300

    [Create] Created Customer entity

---
+++ ModusoftCRM.Domain/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Domain.Entities
+{
+    public class Customer : EntityBase<Guid>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? ErpId { get; set; }
+        public string? Picture { get; set; }
+        
+        // public ICollection<Franchise> Franchises { get; set; }
+
+    }
+}
