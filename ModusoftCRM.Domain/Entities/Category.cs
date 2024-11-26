6c1156ace1f495a26f906474672e5be628c1ea31
Sila Kuscu <silaksc27@gmail.com>
2024-11-26 15:14:20 +0300

    [modified] id type changed to guid

--- ModusoftCRM.Domain/Entities/Category.cs
+++ ModusoftCRM.Domain/Entities/Category.cs
@@ -2,7 +2,7 @@
 
 namespace ModusoftCRM.Domain.Entities
 {
-    public class Category : EntityBase<int>
+    public class Category : EntityBase<Guid>
     {
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
