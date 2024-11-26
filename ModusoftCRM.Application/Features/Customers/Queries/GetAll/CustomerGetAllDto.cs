44c3a0463c2fbde587407e34b9b502d2bd864e06
Sila Kuscu <silaksc27@gmail.com>
2024-11-26 13:54:50 +0300

    [Create] Created Customer get all queries

---
+++ ModusoftCRM.Application/Features/Customers/Queries/GetAll/CustomerGetAllDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ModusoftCRM.Application.Features.Customers.Queries.GetAll
+{
+    public class CustomerGetAllDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? ErpId { get; set; }
+        public string? Picture { get; set; }
+    }
+}
