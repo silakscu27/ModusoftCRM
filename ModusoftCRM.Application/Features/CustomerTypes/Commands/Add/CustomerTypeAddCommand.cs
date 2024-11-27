9fa50ca274d7ad1e4e5fc0687a0e57a1dde6b141
Sila Kuscu <silaksc27@gmail.com>
2024-11-27 16:18:49 +0300

    [Create] Created Customer type add command

---
+++ ModusoftCRM.Application/Features/CustomerTypes/Commands/Add/CustomerTypeAddCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using ModusoftCRM.Domain.Common;
+
+namespace CRM.Application.Features.CustomerTypes.Commands.Add
+{
+    public class CustomerTypeAddCommand : IRequest<Response<int>>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
---
+++ ModusoftCRM.Application/Features/CustomerTypes/Commands/Add/CustomerTypeAddCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace CRM.Application.Features.CustomerTypes.Commands.Add
+{
+    public class CustomerTypeAddCommandValidator : AbstractValidator<CustomerTypeAddCommand>
+    {
+        public CustomerTypeAddCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Müşteri türü ismi boş geçilemez.")
+                .MaximumLength(100)
+                .WithMessage("Müşteri türü ismi en fazla 100 karakter olabilir.");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500)
+                .WithMessage("Açıklama alanı 500 karakterden büyük olamaz.");
+        }
+    }
+}
