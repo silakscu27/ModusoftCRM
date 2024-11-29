1c4d93de928fd30363b1fa8c9823041d57019b70
Sila Kuscu <silaksc27@gmail.com>
2024-11-29 22:48:07 +0300

    [Create] Added files for CompanyDetailGetById functionality

---
+++ ModusoftCRM.Application/Features/CompanyDetails/Queries/GetById/CompanyDetailGetByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Application.Features.CompanyDetails.Queries.GetById
+{
+    public class CompanyDetailGetByIdQuery : IRequest<Response<CompanyDetailGetByIdDto>>
+    {
+        public int Id { get; set; }
+
+        public CompanyDetailGetByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ ModusoftCRM.Application/Features/CompanyDetails/Queries/GetById/CompanyDetailGetByIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace ModusoftCRM.Application.Features.CompanyDetails.Queries.GetById
+{
+    public class CompanyDetailGetByIdQueryValidator : AbstractValidator<CompanyDetailGetByIdQuery>
+    {
+        public CompanyDetailGetByIdQueryValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage("Id must be greater than 0.")
+                .NotEmpty().WithMessage("Id cannot be empty.");
+        }
+    }
+}
