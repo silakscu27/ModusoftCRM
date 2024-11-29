68a8da1b1edfca275f5d6bcfbee2186214840113
Sila Kuscu <silaksc27@gmail.com>
2024-11-29 13:37:20 +0300

    [Create] Created CompanyDetail entity

---
+++ ModusoftCRM.Domain/Entities/CompanyDetail.cs
@@ -0,0 +1,16 @@
+﻿using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Domain.Entities
+{
+    public class CompanyDetail : EntityBase<int> 
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? LegalName { get; set; }
+        public string? Logo { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? LegalAddress { get; set; }
+        public string? Email { get; set; }
+        public bool IsDefault { get; set; }
+
+    }
+}
