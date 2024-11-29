52f10c36864eff949f4ef77c694769eafff5c9df
Sila Kuscu <silaksc27@gmail.com>
2024-11-29 22:47:49 +0300

    [Create] Added files for CompanyDetailGetAll functionality

---
+++ ModusoftCRM.Application/Features/CompanyDetails/Queries/GetAll/CompanyDetailGetAllDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ModusoftCRM.Application.Features.CompanyDetails.Queries.GetAll
+{
+    public class CompanyDetailGetAllDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? LegalName { get; set; }
+        public string? Logo { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? LegalAddress { get; set; }
+        public string? Email { get; set; }
+        public bool IsDefault { get; set; }
+    }
+}
