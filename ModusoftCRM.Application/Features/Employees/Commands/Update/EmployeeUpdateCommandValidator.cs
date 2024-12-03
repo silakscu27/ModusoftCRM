8868ca5c27d6c7f13348daac4904941cc0f6ba6e
Sila Kuscu <silaksc27@gmail.com>
2024-12-03 11:13:41 +0300

    [Create] Created Employee Update commands

---
+++ ModusoftCRM.Application/Features/Employees/Commands/Update/EmployeeUpdateCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace ModusoftCRM.Application.Features.Employees.Commands.Update
+{
+    public class EmployeeUpdateCommandValidator : AbstractValidator<EmployeeUpdateCommand>
+    {
+        public EmployeeUpdateCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Çalışan ID boş olamaz.");
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("Ad boş olamaz.");
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("Soyad boş olamaz.");
+            RuleFor(x => x.Email).NotEmpty().EmailAddress().WithMessage("Geçerli bir e-posta adresi giriniz.");
+            RuleFor(x => x.MobilePhoneNumber).NotEmpty().WithMessage("Telefon numarası boş olamaz.");
+        }
+    }
+}
