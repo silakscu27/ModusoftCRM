a68f3bbfe446474895543ba12d67baeff5b4419f
Sila Kuscu <silaksc27@gmail.com>
2024-12-03 13:34:37 +0300

    [Create] Created Employee GetById queries

---
+++ ModusoftCRM.Application/Features/Employees/Queries/GetById/EmployeeGetByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ModusoftCRM.Application.Common.Interfaces;
+
+namespace ModusoftCRM.Application.Features.Employees.Queries.GetById
+{
+    public class EmployeeGetByIdQueryHandler : IRequestHandler<EmployeeGetByIdQuery, EmployeeGetByIdDto>
+    {
+        private readonly ITenantService _tenantService;
+        private readonly IMapper _mapper;
+
+        public EmployeeGetByIdQueryHandler(ITenantService tenantService, IMapper mapper)
+        {
+            _tenantService = tenantService;
+            _mapper = mapper;
+        }
+
+        public async Task<EmployeeGetByIdDto> Handle(EmployeeGetByIdQuery request, CancellationToken cancellationToken)
+        {
+            var dbContext = await _tenantService.GetDbInstance(cancellationToken);
+
+            var employee = await dbContext.Employees
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+            if (employee == null)
+            {
+                throw new KeyNotFoundException("Çalışan bulunamadı.");
+            }
+
+            return _mapper.Map<EmployeeGetByIdDto>(employee);
+        }
+    }
+}
---
+++ ModusoftCRM.Application/Features/Employees/Queries/GetById/EmployeeGetByIdQueryValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using ModusoftCRM.Application.Common.Interfaces;
+
+namespace ModusoftCRM.Application.Features.Employees.Queries.GetById
+{
+    public class EmployeeGetByIdQueryValidator : AbstractValidator<EmployeeGetByIdQuery>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public EmployeeGetByIdQueryValidator(ITenantService tenantService)
+        {
+            _context = tenantService.GetDbInstance(CancellationToken.None).GetAwaiter().GetResult();
+
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .WithMessage("Lütfen bir çalışan seçiniz.");
+
+            RuleFor(x => x.Id)
+                .MustAsync(AnyEmployeeAsync)
+                .WithMessage("Seçili çalışan bulunamadı.");
+        }
+
+        private Task<bool> AnyEmployeeAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return _context.Employees.AnyAsync(x => x.Id == id, cancellationToken);
+        }
+    }
+}
