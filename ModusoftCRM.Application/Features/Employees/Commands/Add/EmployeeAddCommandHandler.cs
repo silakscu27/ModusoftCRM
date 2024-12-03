17287b143ef2b5883672f79c0c5aa4656bd7dc5d
Sila Kuscu <silaksc27@gmail.com>
2024-12-03 11:13:23 +0300

    [Create] Created Employee Add commands

---
+++ ModusoftCRM.Application/Features/Employees/Commands/Add/EmployeeAddCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using MediatR;
+using ModusoftCRM.Application.Common.Interfaces;
+using ModusoftCRM.Domain.Common;
+using ModusoftCRM.Domain.Entities;
+
+namespace ModusoftCRM.Application.Features.Employees.Commands.Add
+{
+    public class EmployeeAddCommandHandler : IRequestHandler<EmployeeAddCommand, Response<Guid>> // Response<Guid> olarak düzenlendi
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+        private readonly ICurrentUserService _currentUserService;
+
+        public EmployeeAddCommandHandler(
+            IApplicationDbContext dbContext,
+            IMapper mapper,
+            ICurrentUserService currentUserService)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<Response<Guid>> Handle(EmployeeAddCommand request, CancellationToken cancellationToken)
+        {
+            // Employee nesnesi oluşturuluyor
+            var employee = _mapper.Map<Employee>(request);
+
+            // Oluşturma bilgileri ayarlanıyor
+            employee.CreatedByUserId = _currentUserService.UserId
+                ?? throw new ArgumentNullException(nameof(_currentUserService.UserId));
+            employee.CreatedOn = DateTimeOffset.UtcNow;
+
+            // Veritabanına ekleme işlemi
+            await _dbContext.Employees.AddAsync(employee, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            // Yanıt olarak Employee.Id dönülüyor
+            return new Response<Guid>(employee.Id, "Çalışan başarıyla eklenmiştir.");
+        }
+    }
+}
---
+++ ModusoftCRM.Application/Features/Employees/Commands/Add/EmployeeAddCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+
+namespace ModusoftCRM.Application.Features.Employees.Commands.Add
+{
+    public class EmployeeAddCommandValidator : AbstractValidator<EmployeeAddCommand>
+    {
+        public EmployeeAddCommandValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("Ad boş geçilemez.")
+                .MaximumLength(50).WithMessage("Ad 50 karakterden uzun olamaz.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Soyad boş geçilemez.")
+                .MaximumLength(50).WithMessage("Soyad 50 karakterden uzun olamaz.");
+
+            RuleFor(x => x.MobilePhoneNumber)
+                .NotEmpty().WithMessage("Mobil telefon numarası boş geçilemez.")
+                .Matches(@"^\+?\d{10,15}$")
+                .WithMessage("Geçerli bir mobil telefon numarası giriniz.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email adresi boş geçilemez.")
+                .EmailAddress().WithMessage("Geçerli bir email adresi giriniz.");
+
+            RuleFor(x => x.DepartmentId)
+                .GreaterThan(0).When(x => x.DepartmentId.HasValue)
+                .WithMessage("Departman ID geçerli bir değer olmalıdır.");
+        }
+    }
+}
