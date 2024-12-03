b3ea209ecdb4e4fa65f37e4f626e966c2c85abca
Sila Kuscu <silaksc27@gmail.com>
2024-12-03 10:02:47 +0300

    [Create] Created Department and Employee entities

---
+++ ModusoftCRM.Domain/Entities/Department.cs
@@ -0,0 +1,11 @@
+﻿using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Domain.Entities
+{
+    public class Department : EntityBase<int>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public virtual ICollection<Employee>? Employees { get; set; }
+    }
+}
---
+++ ModusoftCRM.Domain/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Domain.Entities
+{
+    public class Employee : EntityBase<Guid>
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string MobilePhoneNumber { get; set; } = string.Empty;
+        public string? WorkPhoneNumber { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public virtual Department? Department { get; set; }
+    }
+}
