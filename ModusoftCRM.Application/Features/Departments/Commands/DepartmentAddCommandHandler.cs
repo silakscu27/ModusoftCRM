3373f8edf9e2de73286a2918da913c05f9fc556f
Sila Kuscu <silaksc27@gmail.com>
2024-12-03 15:01:49 +0300

    [Create] Created Department Add command

---
+++ ModusoftCRM.Application/Features/Departments/Commands/DepartmentAddCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using ModusoftCRM.Application.Common.Interfaces;
+using ModusoftCRM.Domain.Entities;
+
+namespace ModusoftCRM.Application.Features.Departments.Commands.Add
+{
+    public class DepartmentAddCommandHandler : IRequestHandler<DepartmentAddCommand, int>
+    {
+        private readonly ITenantService _tenantService;
+
+        public DepartmentAddCommandHandler(ITenantService tenantService)
+        {
+            _tenantService = tenantService;
+        }
+
+        public async Task<int> Handle(DepartmentAddCommand request, CancellationToken cancellationToken)
+        {
+            var dbContext = await _tenantService.GetDbInstance(cancellationToken);
+
+            var department = new Department
+            {
+                Id = 0, 
+                CreatedByUserId = "1",
+                Name = request.Name,
+                Description = request.Description
+            };
+
+            await dbContext.Departments.AddAsync(department, cancellationToken);
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return department.Id;
+        }
+    }
+}
