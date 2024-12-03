0f161c9d6a3a34cddad9d0e04e002035cb43a077
Sila Kuscu <silaksc27@gmail.com>
2024-12-03 10:49:18 +0300

    [Create] Created Employee Configuration

---
+++ ModusoftCRM.Infrastructure/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ModusoftCRM.Domain.Entities;
+
+namespace ModusoftCRM.Infrastructure.Persistence.Configurations.Application
+{
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            // Primary key
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+            // FirstName configuration
+            builder.Property(x => x.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            // LastName configuration
+            builder.Property(x => x.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            // MobilePhoneNumber configuration
+            builder.Property(x => x.MobilePhoneNumber)
+                .IsRequired()
+                .HasMaxLength(11);
+
+            // WorkPhoneNumber configuration
+            builder.Property(x => x.WorkPhoneNumber)
+                .IsRequired(false)
+                .HasMaxLength(11);
+
+            // Email configuration
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            // Department configuration
+            builder.HasOne(x => x.Department)
+                .WithMany()
+                .HasForeignKey("DepartmentId")
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Auditing fields
+            builder.Property(x => x.CreatedByUserId)
+                .IsRequired()
+                .HasMaxLength(75);
+
+            builder.Property(x => x.CreatedOn)
+                .IsRequired();
+
+            builder.Property(x => x.ModifiedByUserId)
+                .IsRequired(false)
+                .HasMaxLength(75);
+
+            builder.Property(x => x.LastModifiedOn)
+                .IsRequired(false);
+
+            builder.Property(x => x.DeletedByUserId)
+                .IsRequired(false)
+                .HasMaxLength(75);
+
+            builder.Property(x => x.DeletedOn)
+                .IsRequired(false);
+
+            builder.Property(x => x.IsDeleted)
+                .IsRequired()
+                .HasDefaultValue(false);
+
+            // Table mapping
+            builder.ToTable("Employees");
+        }
+    }
+}
