a6430bf3d03df47dd8e2feed0d824effa51a29d3
Sila Kuscu <silaksc27@gmail.com>
2024-12-03 15:00:05 +0300

    [Create] Created Department Add command

---
+++ ModusoftCRM.Application/Features/Departments/Commands/DepartmentAddCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace ModusoftCRM.Application.Features.Departments.Commands.Add
+{
+    public class DepartmentAddCommandValidator : AbstractValidator<DepartmentAddCommand>
+    {
+        public DepartmentAddCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Departman adı boş olamaz.")
+                .MaximumLength(100)
+                .WithMessage("Departman adı en fazla 100 karakter olabilir.");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500)
+                .WithMessage("Açıklama en fazla 500 karakter olabilir.");
+        }
+    }
+}
