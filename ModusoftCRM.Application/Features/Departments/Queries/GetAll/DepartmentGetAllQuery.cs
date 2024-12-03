60943fb88896a5bd611bb032590b46c37f3a32dd
Sila Kuscu <silaksc27@gmail.com>
2024-12-03 15:24:38 +0300

    [Create] Created Department Get All queries

---
+++ ModusoftCRM.Application/Features/Departments/Queries/GetAll/DepartmentGetAllQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace ModusoftCRM.Application.Features.Departments.Queries.GetAll
+{
+    public class DepartmentGetAllQuery : IRequest<List<DepartmentGetAllDto>>
+    {
+    }
+}
---
+++ ModusoftCRM.Application/Features/Departments/Queries/GetAll/DepartmentGetAllQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ModusoftCRM.Application.Common.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ModusoftCRM.Application.Features.Departments.Queries.GetAll
+{
+    public class DepartmentGetAllQueryHandler : IRequestHandler<DepartmentGetAllQuery, List<DepartmentGetAllDto>>
+    {
+        private readonly ITenantService _tenantService;
+        private readonly IMapper _mapper;
+
+        public DepartmentGetAllQueryHandler(ITenantService tenantService, IMapper mapper)
+        {
+            _tenantService = tenantService;
+            _mapper = mapper;
+        }
+
+        public async Task<List<DepartmentGetAllDto>> Handle(DepartmentGetAllQuery request, CancellationToken cancellationToken)
+        {
+            var dbContext = await _tenantService.GetDbInstance(cancellationToken);
+
+            var departments = await dbContext.Departments
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+
+            return _mapper.Map<List<DepartmentGetAllDto>>(departments);
+        }
+    }
+}
