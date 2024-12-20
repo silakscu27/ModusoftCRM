a2b7c05c8009d543748060bd4b3c59eb89e5bfaa
Sila Kuscu <silaksc27@gmail.com>
2024-12-20 18:26:38 +0300

    [Create] Created product get all query

---
+++ ModusoftCRM.Application/Features/Products/Queries/GetAll/ProductGetAllQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace ModusoftCRM.Application.Features.Products.Queries.GetAll
+{
+    public class ProductGetAllQuery : IRequest<List<ProductGetAllDto>>
+    {
+    }
+}
---
+++ ModusoftCRM.Application/Features/Products/Queries/GetAll/ProductGetAllQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ModusoftCRM.Application.Common.Interfaces;
+
+namespace ModusoftCRM.Application.Features.Products.Queries.GetAll
+{
+    public class ProductGetAllQueryHandler : IRequestHandler<ProductGetAllQuery, List<ProductGetAllDto>>
+    {
+        private readonly ITenantService _tenantService;
+        private readonly IMapper _mapper;
+
+        public ProductGetAllQueryHandler(ITenantService tenantService, IMapper mapper)
+        {
+            _tenantService = tenantService;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ProductGetAllDto>> Handle(ProductGetAllQuery request, CancellationToken cancellationToken)
+        {
+            var dbContext = await _tenantService.GetDbInstance(cancellationToken);
+
+            return await dbContext
+                .Products
+                .AsNoTracking()
+                .Select(x => _mapper.Map<ProductGetAllDto>(x))
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
