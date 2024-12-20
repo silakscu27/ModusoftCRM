ae80a26e3947f2511bfaaabf7b4db784b3b6fdb4
Sila Kuscu <silaksc27@gmail.com>
2024-12-20 18:26:49 +0300

    [Create] Created product get by id query

---
+++ ModusoftCRM.Application/Features/Products/Queries/GetById/ProductGetByIdDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ModusoftCRM.Application.Features.Products.Queries.GetById
+{
+    public class ProductGetByIdDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ ModusoftCRM.Application/Features/Products/Queries/GetById/ProductGetByIdQueryValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using ModusoftCRM.Application.Common.Interfaces;
+
+namespace ModusoftCRM.Application.Features.Products.Queries.GetById
+{
+    public class ProductGetByIdQueryValidator : AbstractValidator<ProductGetByIdQuery>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public ProductGetByIdQueryValidator(ITenantService tenantService)
+        {
+            _context = tenantService.GetDbInstance(CancellationToken.None).GetAwaiter().GetResult();
+
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Lütfen bir ürün seçiniz.");
+
+            RuleFor(x => x.Id).MustAsync(AnyProductAsync).WithMessage("Seçili ürün bulunamadı.");
+        }
+
+        private Task<bool> AnyProductAsync(int id, CancellationToken cancellationToken)
+        {
+            return _context.Products.AnyAsync(x => x.Id == id, cancellationToken);
+        }
+    }
+}
