7096e71bfdbfb5e993a6fa9cd2f6cc2a9e333043
Sila Kuscu <silaksc27@gmail.com>
2024-12-24 11:23:37 +0300

    [Create] Created FranchiseType entity

---
+++ ModusoftCRM.Domain/Entities/FranchiseType.cs
@@ -0,0 +1,12 @@
+﻿using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Domain.Entities
+{
+    public class FranchiseType : EntityBase<Guid>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public bool IsDefault { get; set; }
+        //public virtual ICollection<Franchise>? Franchises { get; set; }
+    }
+}
