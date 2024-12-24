a0702747b4ce16e93e94b8db1942f59dfb8ae470
Sila Kuscu <silaksc27@gmail.com>
2024-12-24 17:18:09 +0300

    [Create] Created FranchiseType get all queries

---
+++ ModusoftCRM.Application/Features/FranchiseTypes/Queries/GetAll/FranchiseTypeGetAllDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ModusoftCRM.Application.Features.FranchiseTypes.Queries.GetAll
+{
+    public class FranchiseTypeGetAllDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public bool IsDefault { get; set; }
+    }
+}
---
+++ ModusoftCRM.Application/Features/FranchiseTypes/Queries/GetAll/FranchiseTypeGetAllQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace ModusoftCRM.Application.Features.FranchiseTypes.Queries.GetAll
+{
+    public class FranchiseTypeGetAllQuery : IRequest<List<FranchiseTypeGetAllDto>>
+    {
+    }
+}
