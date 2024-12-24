951dd5588bec3816dc7f59202105a1cec8abc6fd
Sila Kuscu <silaksc27@gmail.com>
2024-12-24 17:19:34 +0300

    [Add] Added relationship between FranchiseType and Franchise tables

--- ModusoftCRM.Domain/Entities/FranchiseType.cs
+++ ModusoftCRM.Domain/Entities/FranchiseType.cs
@@ -7,6 +7,6 @@ namespace ModusoftCRM.Domain.Entities
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public bool IsDefault { get; set; }
-        //public virtual ICollection<Franchise>? Franchises { get; set; }
+        public virtual ICollection<Franchise>? Franchises { get; set; }
     }
 }
