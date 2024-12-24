1d3dbec914a6f2b8306ccce81e077b234f32c4ba
Sila Kuscu <silaksc27@gmail.com>
2024-12-24 17:22:30 +0300

    [Create] Created FranchiseType Controller

---
+++ ModusoftCRM.WebAPI/Controllers/FranchiseTypeController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ModusoftCRM.Application.Features.FranchiseTypes.Queries.GetAll;
+using ModusoftCRM.WebAPI.Abstractions;
+
+namespace ModusoftCRM.WebAPI.Controllers
+{
+    public sealed class FranchiseTypesController : ApiController
+    {
+        public FranchiseTypesController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
+        {
+            var franchiseTypes = await _mediator.Send(new FranchiseTypeGetAllQuery(), cancellationToken);
+            return Ok(franchiseTypes);
+        }
+    }
+}
