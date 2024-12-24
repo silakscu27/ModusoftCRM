c823d31919c39df1faaffdbb2cf5e5d37fed1926
Sila Kuscu <silaksc27@gmail.com>
2024-12-24 17:20:21 +0300

    [Create] Created FranchiseType Configuration

---
+++ ModusoftCRM.Infrastructure/Configurations/FranchiseTypeConfiguration.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ModusoftCRM.Domain.Entities;
+
+namespace ModusoftCRM.Infrastructure.Persistence.Configurations.Application
+{
+    public class FranchiseTypeConfiguration : IEntityTypeConfiguration<FranchiseType>
+    {
+        public void Configure(EntityTypeBuilder<FranchiseType> builder)
+        {
+            // Primary key
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+            // Name configuration
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            // Description configuration
+            builder.Property(x => x.Description)
+                .IsRequired(false)
+                .HasMaxLength(250);
+
+            // IsDefault configuration
+            builder.Property(x => x.IsDefault)
+                .IsRequired()
+                .HasDefaultValue(false);
+
+            // Franchises relationship configuration
+            builder.HasMany(x => x.Franchises)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Auditing fields
+            builder.Property(x => x.CreatedByUserId)
+                .IsRequired()
+                .HasMaxLength(75);
+
+            builder.Property(x => x.CreatedOn)
+                .IsRequired();
+
+            builder.Property(x => x.ModifiedByUserId)
+                .IsRequired(false)
+                .HasMaxLength(75);
+
+            builder.Property(x => x.LastModifiedOn)
+                .IsRequired(false);
+
+            builder.Property(x => x.DeletedByUserId)
+                .IsRequired(false)
+                .HasMaxLength(75);
+
+            builder.Property(x => x.DeletedOn)
+                .IsRequired(false);
+
+            builder.Property(x => x.IsDeleted)
+                .IsRequired()
+                .HasDefaultValue(false);
+
+            // Table mapping
+            builder.ToTable("FranchiseTypes");
+        }
+    }
+}
