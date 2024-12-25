386fe7e067f775a050033ebe4e9460ef1647bea2
Sila Kuscu <silaksc27@gmail.com>
2024-12-25 17:56:05 +0300

    [Create] Created Franchise Get All Query

---
+++ ModusoftCRM.Application/Features/Franchises/Queries/GetAll/FranchiseGetAllQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace ModusoftCRM.Application.Features.Franchises.Queries.GetAll
+{
+    public class FranchiseGetAllQuery : IRequest<List<FranchiseGetAllDto>>
+    {
+    }
+}
