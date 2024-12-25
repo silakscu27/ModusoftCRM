94fef8df0e8f34683b5462fb1496f7966887a05c
Sila Kuscu <silaksc27@gmail.com>
2024-12-25 17:56:32 +0300

    [Create] Created Franchise GetById Query

---
+++ ModusoftCRM.Application/Features/Franchises/Queries/GetById/FranchiseGetByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ModusoftCRM.Application.Common.Interfaces;
+
+namespace ModusoftCRM.Application.Features.Franchises.Queries.GetById
+{
+    public class FranchiseGetByIdQueryHandler : IRequestHandler<FranchiseGetByIdQuery, FranchiseGetByIdDto>
+    {
+        private readonly ITenantService _tenantService;
+        private readonly IMapper _mapper;
+
+        public FranchiseGetByIdQueryHandler(ITenantService tenantService, IMapper mapper)
+        {
+            _tenantService = tenantService;
+            _mapper = mapper;
+        }
+
+        public async Task<FranchiseGetByIdDto> Handle(FranchiseGetByIdQuery request, CancellationToken cancellationToken)
+        {
+            var dbContext = await _tenantService.GetDbInstance(cancellationToken);
+
+            var franchise = await dbContext.Franchises
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+            if (franchise == null)
+            {
+                throw new KeyNotFoundException("Franchise bulunamadı.");
+            }
+
+            return _mapper.Map<FranchiseGetByIdDto>(franchise);
+        }
+    }
+}
