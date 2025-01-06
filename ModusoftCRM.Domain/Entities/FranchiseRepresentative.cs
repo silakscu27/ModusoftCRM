b5cc1be8abce062335a17e9308dce3e03cbc6368
Sila Kuscu <silaksc27@gmail.com>
2025-01-06 23:19:42 +0300

    [Create] Created Franschise Rep entity

---
+++ ModusoftCRM.Domain/Entities/FranchiseRepresentative.cs
@@ -0,0 +1,22 @@
+﻿using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Domain.Entities
+{
+    public class FranchiseRepresentative : EntityBase<Guid>
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string? Description { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? MobilePhoneNumber { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? LinkedIn { get; set; }
+
+        public Guid FranchiseId { get; set; }
+        public virtual Franchise? Franchise { get; set; }
+        //public virtual ICollection<FranchiseRepresentativeHistory> FranchiseRepresentativeHistories { get; set; } = new List<FranchiseRepresentativeHistory>();
+    }
+}
