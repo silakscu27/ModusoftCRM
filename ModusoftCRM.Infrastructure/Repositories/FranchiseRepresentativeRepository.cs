d13003db50b6d3541a6190dd0c24432fd3a9e8b5
Sila Kuscu <silaksc27@gmail.com>
2025-01-06 23:22:31 +0300

    [Create] Created Franschise Rep interface repo

---
+++ ModusoftCRM.Infrastructure/Repositories/FranchiseRepresentativeRepository.cs
@@ -0,0 +1,12 @@
+﻿using ModusoftCRM.Domain.Entities;
+using ModusoftCRM.Domain.Repositories;
+using ModusoftCRM.Infrastructure.Context;
+using GenericRepository;
+
+namespace ModusoftCRM.Infrastructure.Repositories;
+internal sealed class FranchiseRepresentativeRepository : Repository<FranchiseRepresentative, ApplicationDbContext>, IFranchiseRepresentativeRepository
+{
+    public FranchiseRepresentativeRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
