b243cd57d8f9809daa86e2a674296b4a4eb1ef36
Sila Kuscu <silaksc27@gmail.com>
2025-01-06 23:17:01 +0300

    [Create] Created Franchise Rep add command

---
+++ ModusoftCRM.Application/Features/FranchiseRepresentatives/Commands/Add/FranchiseRepresentativeAddCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using MediatR;
+using ModusoftCRM.Application.Common.Interfaces;
+using ModusoftCRM.Domain.Common;
+using ModusoftCRM.Domain.Entities;
+
+namespace ModusoftCRM.Application.Features.FranchiseRepresentatives.Commands.Add
+{
+    public class FranchiseRepresentativeAddCommandHandler : IRequestHandler<FranchiseRepresentativeAddCommand, Response<Guid>>
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+        private readonly ICurrentUserService _currentUserService;
+
+        public FranchiseRepresentativeAddCommandHandler(
+            IApplicationDbContext dbContext,
+            IMapper mapper,
+            ICurrentUserService currentUserService)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<Response<Guid>> Handle(FranchiseRepresentativeAddCommand request, CancellationToken cancellationToken)
+        {
+            var franchiseRepresentative = _mapper.Map<FranchiseRepresentative>(request);
+
+            franchiseRepresentative.CreatedByUserId = _currentUserService.UserId
+                ?? throw new ArgumentNullException(nameof(_currentUserService.UserId));
+            franchiseRepresentative.CreatedOn = DateTimeOffset.UtcNow;
+
+            await _dbContext.FranchiseRepresentatives.AddAsync(franchiseRepresentative, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return new Response<Guid>(franchiseRepresentative.Id, "Franchise temsilcisi başarıyla eklenmiştir.");
+        }
+    }
+}
