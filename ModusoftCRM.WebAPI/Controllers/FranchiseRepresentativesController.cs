e474f8490c130d0e1961010d5cacece79bb048af
Sila Kuscu <silaksc27@gmail.com>
2025-01-06 23:22:47 +0300

    [Create] Created Franschise Rep controller

---
+++ ModusoftCRM.WebAPI/Controllers/FranchiseRepresentativesController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ModusoftCRM.Application.Features.FranchiseRepresentatives.Commands.Add;
+using ModusoftCRM.Application.Features.FranchiseRepresentatives.Queries.GetAll;
+using ModusoftCRM.WebAPI.Abstractions;
+
+namespace ModusoftCRM.WebApi.Controllers
+{
+    public sealed class FranchiseRepresentativesController : ApiController
+    {
+        public FranchiseRepresentativesController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAsync(FranchiseRepresentativeAddCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
+        {
+            var representatives = await _mediator.Send(new FranchiseRepresentativeGetAllQuery(), cancellationToken);
+            return Ok(representatives);
+        }
+    }
+}
