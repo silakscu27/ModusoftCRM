371db4a513097630ce4ac654e833e6f30791cd80
Sila Kuscu <silaksc27@gmail.com>
2025-01-07 18:22:57 +0300

    [Create] Created Country get all query

---
+++ ModusoftCRM.Application/Features/Countries/Queries/GetAll/CountryGetAllDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ModusoftCRM.Application.Features.Countries.Queries.GetAll
+{
+    public class CountryGetAllDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+    }
+}
---
+++ ModusoftCRM.Application/Features/Countries/Queries/GetAll/CountryGetAllQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace ModusoftCRM.Application.Features.Countries.Queries.GetAll
+{
+    public class CountryGetAllQuery : IRequest<List<CountryGetAllDto>>
+    {
+    }
+}
