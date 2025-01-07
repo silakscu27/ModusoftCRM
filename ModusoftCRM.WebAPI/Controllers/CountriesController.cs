e5a86a3b678ee94d5ee2289c784a4a2c76e7b7a9
Sila Kuscu <silaksc27@gmail.com>
2025-01-07 22:58:07 +0300

    [Create] Created Country Controller

---
+++ ModusoftCRM.WebAPI/Controllers/CountriesController.cs
@@ -0,0 +1,54 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ModusoftCRM.Application.Features.Countries.Commands.Add;
+using ModusoftCRM.Application.Features.Countries.Commands.Update;
+using ModusoftCRM.Application.Features.Countries.Queries.GetAll;
+using ModusoftCRM.Application.Features.Countries.Queries.GetById;
+using ModusoftCRM.WebAPI.Abstractions;
+using TS.Result;
+
+namespace ModusoftCRM.WebApi.Controllers
+{
+    public sealed class CountriesController : ApiController
+    {
+        public CountriesController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAsync(CountryAddCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync(CountryGetAllQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            var request = new CountryGetByIdQuery(id);
+            var response = await _mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync(CountryUpdateCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(id, cancellationToken);
+            return Ok(response);
+        }
+    }
+}
