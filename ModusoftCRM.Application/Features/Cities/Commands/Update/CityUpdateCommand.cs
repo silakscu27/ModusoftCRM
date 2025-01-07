3f91f77a9bf6c4a5fc5578c03f465746b3a48319
Sila Kuscu <silaksc27@gmail.com>
2025-01-07 18:20:31 +0300

    [Create] Created City get all command

---
+++ ModusoftCRM.Application/Features/Cities/Commands/Update/CityUpdateCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Application.Features.Cities.Commands.Update
+{
+    public class CityUpdateCommand : IRequest<Response<int>>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+        public int CountryId { get; set; }
+    }
+}
---
+++ ModusoftCRM.Application/Features/Cities/Queries/GetAll/CityGetAllDtoHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ModusoftCRM.Application.Common.Interfaces;
+
+namespace ModusoftCRM.Application.Features.Cities.Queries.GetAll
+{
+    public class CityGetAllQueryHandler : IRequestHandler<CityGetAllQuery, List<CityGetAllDto>>
+    {
+        private readonly ITenantService _tenantService;
+        private readonly IMapper _mapper;
+
+        public CityGetAllQueryHandler(ITenantService tenantService, IMapper mapper)
+        {
+            _tenantService = tenantService;
+            _mapper = mapper;
+        }
+
+        public async Task<List<CityGetAllDto>> Handle(CityGetAllQuery request, CancellationToken cancellationToken)
+        {
+            var dbContext = await _tenantService.GetDbInstance(cancellationToken);
+
+            return await dbContext
+                .Cities
+                .Include(c => c.Country) 
+                .AsNoTracking()
+                .Select(city => new CityGetAllDto
+                {
+                    Id = city.Id,
+                    Name = city.Name,
+                    Latitude = city.Latitude,
+                    Longitude = city.Longitude,
+                    CountryName = city.Country.Name 
+                })
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
