841da517e5095a98cceee8674b860cef78b3cf0f
Sila Kuscu <silaksc27@gmail.com>
2025-01-07 22:57:09 +0300

    [Create] Created City Repository

---
+++ ModusoftCRM.Infrastructure/Repositories/CityRepository.cs
@@ -0,0 +1,12 @@
+﻿using ModusoftCRM.Domain.Entities;
+using ModusoftCRM.Domain.Repositories;
+using ModusoftCRM.Infrastructure.Context;
+using GenericRepository;
+
+namespace ModusoftCRM.Infrastructure.Repositories;
+internal sealed class CityRepository : Repository<City, ApplicationDbContext>, ICityRepository
+{
+    public CityRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
