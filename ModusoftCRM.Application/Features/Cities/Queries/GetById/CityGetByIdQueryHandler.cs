e54389a3d968cd41415e2bda7226aa202dedafa0
Sila Kuscu <silaksc27@gmail.com>
2025-01-07 18:20:51 +0300

    [Create] Created City get by id query

---
+++ ModusoftCRM.Application/Features/Cities/Queries/GetById/CityGetByIdQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ModusoftCRM.Application.Common.Interfaces;
+using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Application.Features.Cities.Queries.GetById
+{
+    public class CityGetByIdQueryHandler : IRequestHandler<CityGetByIdQuery, Response<CityGetByIdDto>>
+    {
+        private readonly ITenantService _tenantService;
+        private readonly IMapper _mapper;
+
+        public CityGetByIdQueryHandler(ITenantService tenantService, IMapper mapper)
+        {
+            _tenantService = tenantService;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<CityGetByIdDto>> Handle(CityGetByIdQuery request, CancellationToken cancellationToken)
+        {
+            var dbContext = await _tenantService.GetDbInstance(cancellationToken);
+
+            var city = await dbContext.Cities
+                .Include(c => c.Country) 
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+            if (city == null)
+            {
+                return new Response<CityGetByIdDto>("City not found");
+            }
+
+            var cityDto = _mapper.Map<CityGetByIdDto>(city);
+            return new Response<CityGetByIdDto>(cityDto);
+        }
+    }
+}
