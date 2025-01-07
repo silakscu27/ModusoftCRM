be12dc13104883baf6bdf3e8458e445f88777807
Sila Kuscu <silaksc27@gmail.com>
2025-01-07 18:22:39 +0300

    [Create] Created Country update command

---
+++ ModusoftCRM.Application/Features/Countries/Commands/Update/CountryUpdateCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Application.Features.Countries.Commands.Update
+{
+    public class CountryUpdateCommand : IRequest<Response<int>>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+    }
+}
