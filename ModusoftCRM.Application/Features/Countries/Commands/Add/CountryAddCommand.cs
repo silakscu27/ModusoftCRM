ae070705ed9cd0a20f1b80bf40701559ba8df551
Sila Kuscu <silaksc27@gmail.com>
2025-01-07 18:22:26 +0300

    [Create] Created Country add command

---
+++ ModusoftCRM.Application/Features/Countries/Commands/Add/CountryAddCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Application.Features.Countries.Commands.Add
+{
+    public class CountryAddCommand : IRequest<Response<int>>
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+    }
+}
---
+++ ModusoftCRM.Application/Features/Countries/Commands/Add/CountryAddCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace ModusoftCRM.Application.Features.Countries.Commands.Add
+{
+    public class CountryAddCommandValidator : AbstractValidator<CountryAddCommand>
+    {
+        public CountryAddCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Ad alanı boş geçilemez.")
+                .MaximumLength(100)
+                .WithMessage("Ad alanı 100 karakterden büyük olamaz.");
+
+            RuleFor(x => x.Latitude)
+                .InclusiveBetween(-90, 90)
+                .WithMessage("Enlem -90 ile 90 arasında olmalıdır.");
+
+            RuleFor(x => x.Longitude)
+                .InclusiveBetween(-180, 180)
+                .WithMessage("Boylam -180 ile 180 arasında olmalıdır.");
+        }
+    }
+}
