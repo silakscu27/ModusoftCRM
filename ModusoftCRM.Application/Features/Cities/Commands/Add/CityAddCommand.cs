2a994a0ce5b4f6a2aa1ae7efb7c35c6037fef2b1
Sila Kuscu <silaksc27@gmail.com>
2025-01-07 18:18:55 +0300

    [Create] Created City add command

---
+++ ModusoftCRM.Application/Features/Cities/Commands/Add/CityAddCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Application.Features.Cities.Commands.Add
+{
+    public class CityAddCommand : IRequest<Response<int>>
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+        public int CountryId { get; set; }
+    }
+}
+    
---
+++ ModusoftCRM.Application/Features/Cities/Commands/Add/CityAddCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace ModusoftCRM.Application.Features.Cities.Commands.Add
+{
+    public class CityAddCommandValidator : AbstractValidator<CityAddCommand>
+    {
+        public CityAddCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Şehir adı boş geçilemez.")
+                .MaximumLength(100).WithMessage("Şehir adı 100 karakterden uzun olamaz.");
+
+            RuleFor(x => x.Latitude)
+                .InclusiveBetween(-90, 90)
+                .When(x => x.Latitude.HasValue)
+                .WithMessage("Enlem değeri -90 ile 90 arasında olmalıdır.");
+
+            RuleFor(x => x.Longitude)
+                .InclusiveBetween(-180, 180)
+                .When(x => x.Longitude.HasValue)
+                .WithMessage("Boylam değeri -180 ile 180 arasında olmalıdır.");
+
+            RuleFor(x => x.CountryId)
+                .GreaterThan(0).WithMessage("Geçerli bir ülke ID'si giriniz.");
+        }
+    }
+}
