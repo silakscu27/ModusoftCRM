67be47f10726337cfc82f7a13b2a3ac6678df96a
Sila Kuscu <silaksc27@gmail.com>
2025-01-07 18:13:49 +0300

    [Add] Added City dbset

--- ModusoftCRM.Infrastructure/Context/ApplicationDbContext.cs
+++ ModusoftCRM.Infrastructure/Context/ApplicationDbContext.cs
@@ -23,6 +23,7 @@ namespace ModusoftCRM.Infrastructure.Context
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Country> Countries { get; set; }
+        public DbSet<City> Cities { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<FranchiseType> FranchiseTypes { get; set; }
         public DbSet<Franchise> Franchises { get; set; }
