102b71659f030341004a38eff2a9b2f654a1d9cd
Sila Kuscu <silaksc27@gmail.com>
2025-01-07 18:17:24 +0300

    [Create] Created City entity

---
+++ ModusoftCRM.Domain/Entities/City.cs
@@ -0,0 +1,15 @@
+﻿using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Domain.Entities
+{
+    public class City : EntityBase<int>
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+
+        // Relationship
+        public int CountryId { get; set; }
+        public Country Country { get; set; } = null!;
+    }
+}
