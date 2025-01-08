5bf14c786f7b45dee6cf5818f1082b6f656c0679
Sila Kuscu <silaksc27@gmail.com>
2025-01-08 14:31:08 +0300

    [Create] Created FranchiseRepHistory entity

---
+++ ModusoftCRM.Domain/Entities/FranchiseRepresentativeHistory.cs
@@ -0,0 +1,22 @@
+﻿using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Domain.Entities
+{
+    public class FranchiseRepresentativeHistory : EntityBase<int>
+    {
+        public Guid FranchiseRepresentativeId { get; set; }
+
+        public Guid FranchiseId { get; set; }
+
+        public DateTimeOffset From { get; set; }
+        public DateTimeOffset To { get; set; }
+
+        public string Role { get; set; } = string.Empty;
+        public string? Description { get; set; }
+
+        public Franchise Franchise { get; set; } = null!;
+
+        public FranchiseRepresentative FranchiseRepresentative { get; set; } = null!;
+
+    }
+}
