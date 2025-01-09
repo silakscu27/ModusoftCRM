6f1b94f88a4d322aafde7c622d59504cb2fbcd2b
Sila Kuscu <silaksc27@gmail.com>
2025-01-09 16:20:06 +0300

    [Create] Created FranchiseRepHistory Add Command

---
+++ ModusoftCRM.Application/Features/FranchiseRepHistories/Commands/Add/FranchiseRepHistoryAddCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Application.Features.FranchiseRepHistories.Commands.Add
+{
+    public class FranchiseRepHistoryAddCommand : IRequest<Response<int>>
+    {
+        public Guid FranchiseRepresentativeId { get; set; }
+        public Guid FranchiseId { get; set; }
+        public DateTimeOffset From { get; set; }
+        public DateTimeOffset To { get; set; }
+        public string Role { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
---
+++ ModusoftCRM.Application/Features/FranchiseRepHistories/Commands/Add/FranchiseRepHistoryAddCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using MediatR;
+using ModusoftCRM.Application.Common.Interfaces;
+using ModusoftCRM.Domain.Common;
+using ModusoftCRM.Domain.Entities;
+
+namespace ModusoftCRM.Application.Features.FranchiseRepHistories.Commands.Add
+{
+    public class FranchiseRepHistoryAddCommandHandler : IRequestHandler<FranchiseRepHistoryAddCommand, Response<int>>
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+        private readonly ICurrentUserService _currentUserService;
+
+        public FranchiseRepHistoryAddCommandHandler(
+            IApplicationDbContext dbContext,
+            IMapper mapper,
+            ICurrentUserService currentUserService)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<Response<int>> Handle(FranchiseRepHistoryAddCommand request, CancellationToken cancellationToken)
+        {
+            // Entity mapping
+            var franchiseRepHistory = _mapper.Map<FranchiseRepresentativeHistory>(request);
+
+            // Creation info
+            franchiseRepHistory.CreatedByUserId = _currentUserService.UserId
+                ?? throw new ArgumentNullException(nameof(_currentUserService.UserId));
+            franchiseRepHistory.CreatedOn = DateTimeOffset.UtcNow;
+
+            // Add to database
+            await _dbContext.FranchiseRepresentativeHistories.AddAsync(franchiseRepHistory, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            // Return response
+            return new Response<int>(franchiseRepHistory.Id, "Franchise temsilci geçmişi başarıyla eklenmiştir.");
+        }
+    }
+}
