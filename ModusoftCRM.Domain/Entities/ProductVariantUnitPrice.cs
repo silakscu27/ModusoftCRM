b01baf7021a4baa29433f4c06caf1f5cb757a169
Sila Kuscu <silaksc27@gmail.com>
2025-01-10 18:26:49 +0300

    [Create] Created ProductVariantUnitPrice entity

---
+++ ModusoftCRM.Domain/Entities/ProductVariantUnitPrice.cs
@@ -0,0 +1,15 @@
+﻿using ModusoftCRM.Domain.Common;
+using ModusoftCRM.Domain.Enums;
+
+namespace ModusoftCRM.Domain.Entities
+{
+    public class ProductVariantUnitPrice : EntityBase<int>
+    {
+        public string ProductVariantId { get; set; } = string.Empty;
+        public Guid FranchiseTypeId { get; set; }
+        public string FranchiseTypeName { get; set; } = string.Empty;
+        public DiscountType DiscountType { get; set; }
+        public double DiscountAmount { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
