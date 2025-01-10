fabe6c0e897dca3814d89b16decf989432ed99a2
Sila Kuscu <silaksc27@gmail.com>
2025-01-10 22:26:07 +0300

    [Create] Created ProductVariantUnitPrice Add Command

---
+++ ModusoftCRM.Application/Features/ProductVariantUnitPrices/Commands/Add/ProductVariantUnitPriceAddCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MediatR;
+using ModusoftCRM.Application.Common.Interfaces;
+using ModusoftCRM.Domain.Common;
+using ModusoftCRM.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ModusoftCRM.Application.Features.ProductVariantUnitPrices.Commands.Add
+{
+    public class ProductVariantUnitPriceAddCommandHandler : IRequestHandler<ProductVariantUnitPriceAddCommand, Response<int>>
+    {
+        private readonly ITenantService _tenantService;
+        private readonly IMapper _mapper;
+        private readonly ICurrentUserService _currentUserService;
+
+        public ProductVariantUnitPriceAddCommandHandler(ITenantService tenantService, IMapper mapper, ICurrentUserService currentUserService)
+        {
+            _tenantService = tenantService;
+            _mapper = mapper;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<Response<int>> Handle(ProductVariantUnitPriceAddCommand request, CancellationToken cancellationToken)
+        {
+            var dbContext = await _tenantService.GetDbInstance(cancellationToken);
+
+            var productVariantUnitPrice = _mapper.Map<ProductVariantUnitPrice>(request);
+
+            productVariantUnitPrice.CreatedByUserId = _currentUserService.UserId ?? throw new ArgumentNullException(nameof(_currentUserService.UserId));
+
+            await dbContext.ProductVariantUnitPrices.AddAsync(productVariantUnitPrice, cancellationToken);
+
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return new Response<int>(productVariantUnitPrice.Id, "Fiyat başarıyla eklenmiştir.");
+        }
+    }
+}
---
+++ ModusoftCRM.Application/Features/ProductVariantUnitPrices/Commands/Add/ProductVariantUnitPriceAddCommandValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using ModusoftCRM.Application.Features.ProductVariantUnitPrices.Commands.Add;
+
+namespace ModusoftCRM.Application.Features.ProductVariantUnitPrices.Commands.Add
+{
+    public class ProductVariantUnitPriceAddCommandValidator : AbstractValidator<ProductVariantUnitPriceAddCommand>
+    {
+        public ProductVariantUnitPriceAddCommandValidator()
+        {
+            RuleFor(x => x.ProductVariantId)
+                .NotEmpty()
+                .WithMessage("Ürün çeşit ID'si boş geçilemez.");
+
+            RuleFor(x => x.FranchiseTypeId)
+                .NotEmpty()
+                .WithMessage("Franchise tipi seçilmelidir.");
+
+            RuleFor(x => x.FranchiseTypeName)
+                .NotEmpty()
+                .WithMessage("Franchise tipi adı boş geçilemez.")
+                .MaximumLength(100)
+                .WithMessage("Franchise tipi adı 100 karakterden fazla olamaz.");
+
+            RuleFor(x => x.DiscountType)
+                .IsInEnum()
+                .WithMessage("Geçersiz indirim türü.");
+
+            RuleFor(x => x.DiscountAmount)
+                .GreaterThan(0)
+                .WithMessage("İndirim miktarı sıfırdan büyük olmalıdır.");
+
+            RuleFor(x => x.UnitPrice)
+                .GreaterThan(0)
+                .WithMessage("Birim fiyat sıfırdan büyük olmalıdır.");
+        }
+    }
+}
