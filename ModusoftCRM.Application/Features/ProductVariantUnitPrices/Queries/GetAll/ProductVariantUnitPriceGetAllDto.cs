d52ebc2f63346418dcde64d8088bc77475997858
Sila Kuscu <silaksc27@gmail.com>
2025-01-10 22:26:51 +0300

    [Create] Created ProductVariantUnitPrice Get All Query

---
+++ ModusoftCRM.Application/Features/ProductVariantUnitPrices/Queries/GetAll/ProductVariantUnitPriceGetAllDto.cs
@@ -0,0 +1,15 @@
+﻿using ModusoftCRM.Domain.Enums;
+
+namespace ModusoftCRM.Application.Features.ProductVariantUnitPrices.Queries.GetAll
+{
+    public class ProductVariantUnitPriceGetAllDto
+    {
+        public int Id { get; set; }
+        public string ProductVariantId { get; set; } = string.Empty;
+        public Guid FranchiseTypeId { get; set; }
+        public string FranchiseTypeName { get; set; } = string.Empty;
+        public DiscountType DiscountType { get; set; }
+        public double DiscountAmount { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ ModusoftCRM.Application/Features/ProductVariantUnitPrices/Queries/GetAll/ProductVariantUnitPriceGetAllQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ModusoftCRM.Application.Common.Interfaces;
+
+namespace ModusoftCRM.Application.Features.ProductVariantUnitPrices.Queries.GetAll
+{
+    public class ProductVariantUnitPriceGetAllQueryHandler : IRequestHandler<ProductVariantUnitPriceGetAllQuery, List<ProductVariantUnitPriceGetAllDto>>
+    {
+        private readonly ITenantService _tenantService;
+        private readonly IMapper _mapper;
+
+        public ProductVariantUnitPriceGetAllQueryHandler(ITenantService tenantService, IMapper mapper)
+        {
+            _tenantService = tenantService;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ProductVariantUnitPriceGetAllDto>> Handle(ProductVariantUnitPriceGetAllQuery request, CancellationToken cancellationToken)
+        {
+            var dbContext = await _tenantService.GetDbInstance(cancellationToken);
+
+            return await dbContext
+                .ProductVariantUnitPrices
+                .AsNoTracking()
+                .Select(x => _mapper.Map<ProductVariantUnitPriceGetAllDto>(x))
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
