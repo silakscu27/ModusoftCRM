0d0579f8fdaa0b3e823bc36b83eaeb13fc4eedea
Sila Kuscu <silaksc27@gmail.com>
2025-01-10 22:26:29 +0300

    [Create] Created ProductVariantUnitPrice Update Command

---
+++ ModusoftCRM.Application/Features/ProductVariantUnitPrices/Commands/Update/ProductVariantUnitPriceUpdateCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using MediatR;
+using ModusoftCRM.Application.Common.Interfaces;
+using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Application.Features.ProductVariantUnitPrices.Commands.Update
+{
+    public class ProductVariantUnitPriceUpdateCommandHandler : IRequestHandler<ProductVariantUnitPriceUpdateCommand, Response<int>>
+    {
+        private readonly ITenantService _tenantService;
+        private readonly IMapper _mapper;
+
+        public ProductVariantUnitPriceUpdateCommandHandler(ITenantService tenantService, IMapper mapper)
+        {
+            _tenantService = tenantService;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<int>> Handle(ProductVariantUnitPriceUpdateCommand request, CancellationToken cancellationToken)
+        {
+            var dbContext = await _tenantService.GetDbInstance(cancellationToken);
+
+            var productVariantUnitPrice = await dbContext.ProductVariantUnitPrices.FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (productVariantUnitPrice == null)
+            {
+                return new Response<int>(0, "Ürün varyantı birim fiyatı bulunamadı.");
+            }
+
+            productVariantUnitPrice.ProductVariantId = request.ProductVariantId;
+            productVariantUnitPrice.FranchiseTypeId = request.FranchiseTypeId;
+            productVariantUnitPrice.FranchiseTypeName = request.FranchiseTypeName;
+            productVariantUnitPrice.DiscountType = request.DiscountType;
+            productVariantUnitPrice.DiscountAmount = request.DiscountAmount;
+            productVariantUnitPrice.UnitPrice = request.UnitPrice;
+
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return new Response<int>(productVariantUnitPrice.Id, "Ürün varyantı birim fiyatı başarıyla güncellendi.");
+        }
+    }
+}
---
+++ ModusoftCRM.Application/Features/ProductVariantUnitPrices/Commands/Update/ProductVariantUnitPriceUpdateCommandValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+
+namespace ModusoftCRM.Application.Features.ProductVariantUnitPrices.Commands.Update
+{
+    public class ProductVariantUnitPriceUpdateCommandValidator : AbstractValidator<ProductVariantUnitPriceUpdateCommand>
+    {
+        public ProductVariantUnitPriceUpdateCommandValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0)
+                .WithMessage("Geçerli bir ID girilmelidir.");
+
+            RuleFor(x => x.ProductVariantId)
+                .NotEmpty()
+                .WithMessage("Ürün çeşidi ID'si boş geçilemez.");
+
+            RuleFor(x => x.FranchiseTypeId)
+                .NotEmpty()
+                .WithMessage("Franchise tipi ID'si boş geçilemez.");
+
+            RuleFor(x => x.FranchiseTypeName)
+                .NotEmpty()
+                .WithMessage("Franchise tipi adı boş geçilemez.")
+                .MaximumLength(100)
+                .WithMessage("Franchise tipi adı 100 karakterden fazla olamaz.");
+
+            RuleFor(x => x.DiscountType)
+                .IsInEnum()
+                .WithMessage("Geçersiz indirim türü.");
+
+            RuleFor(x => x.DiscountAmount)
+                .GreaterThanOrEqualTo(0)
+                .WithMessage("İndirim miktarı sıfırdan küçük olamaz.");
+
+            RuleFor(x => x.UnitPrice)
+                .GreaterThan(0)
+                .WithMessage("Birim fiyat sıfırdan büyük olmalıdır.");
+        }
+    }
+}
