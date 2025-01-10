563db8198a11515646ad7f598b64876de8560b49
Sila Kuscu <silaksc27@gmail.com>
2025-01-10 18:23:27 +0300

    [create] Created FranchiseRepHistory Get By Id Query

---
+++ ModusoftCRM.Application/Features/FranchiseRepHistories/Queries/GetById/FranchiseRepHistoryGetByIdDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ModusoftCRM.Application.Features.FranchiseRepHistories.Queries.GetById
+{
+    public class FranchiseRepHistoryGetByIdDto
+    {
+        public int Id { get; set; }
+        public Guid FranchiseRepresentativeId { get; set; }
+        public Guid FranchiseId { get; set; }
+        public DateTimeOffset From { get; set; }
+        public DateTimeOffset To { get; set; }
+        public string Role { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
---
+++ ModusoftCRM.Application/Features/FranchiseRepHistories/Queries/GetById/FranchiseRepHistoryGetByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace ModusoftCRM.Application.Features.FranchiseRepHistories.Queries.GetById
+{
+    public class FranchiseRepHistoryGetByIdQuery : IRequest<FranchiseRepHistoryGetByIdDto>
+    {
+        public int Id { get; set; }
+
+        public FranchiseRepHistoryGetByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
