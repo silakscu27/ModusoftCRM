98594c640707671a05b87729fe5012375b53c752
Sila Kuscu <silaksc27@gmail.com>
2025-01-10 18:23:11 +0300

    [create] Created FranchiseRepHistory Get All Query

---
+++ ModusoftCRM.Application/Features/FranchiseRepHistories/Queries/GetAll/FranchiseRepHistoryGetAllDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ModusoftCRM.Application.Features.FranchiseRepHistories.Queries.GetAll
+{
+    public class FranchiseRepHistoryGetAllDto
+    {
+        public int Id { get; set; }
+        public Guid FranchiseRepresentativeId { get; set; }
+        public Guid FranchiseId { get; set; }
+        public DateTimeOffset From { get; set; }
+        public DateTimeOffset To { get; set; }
+        public string Role { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
