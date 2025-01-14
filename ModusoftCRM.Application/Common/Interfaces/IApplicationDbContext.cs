31e64540c412969ef5c5081dd6316dc3f0cda11b
Sila Kuscu <silaksc27@gmail.com>
2025-01-14 15:32:43 +0300

    [Add] Added ProductVariant dbset

--- ModusoftCRM.Application/Common/Interfaces/IApplicationDbContext.cs
+++ ModusoftCRM.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace ModusoftCRM.Application.Common.Interfaces
         public DbSet<Country> Countries { get; set; }
         public DbSet<City> Cities { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductVariant> ProductVariants { get; set; }
         public DbSet<FranchiseType> FranchiseTypes { get; set; }
         public DbSet<Franchise> Franchises { get; set; }
         public DbSet<FranchiseRepresentative> FranchiseRepresentatives { get; set; }
--- ModusoftCRM.Infrastructure/Context/ApplicationDbContext.cs
+++ ModusoftCRM.Infrastructure/Context/ApplicationDbContext.cs
@@ -25,6 +25,7 @@ namespace ModusoftCRM.Infrastructure.Context
         public DbSet<Country> Countries { get; set; }
         public DbSet<City> Cities { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductVariant> ProductVariants { get; set; }
         public DbSet<FranchiseType> FranchiseTypes { get; set; }
         public DbSet<Franchise> Franchises { get; set; }
         public DbSet<FranchiseRepresentative> FranchiseRepresentatives { get; set; }
