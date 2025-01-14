33801364661adf477daac59f5f6b92d03a1be5ee
Sila Kuscu <silaksc27@gmail.com>
2025-01-14 15:31:45 +0300

    [Create] Created ProductVariant GetById Queries

---
+++ ModusoftCRM.Application/Features/ProductVariants/Queries/GetById/ProductVariantGetByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace ModusoftCRM.Application.Features.ProductVariants.Queries.GetById
+{
+    public class ProductVariantGetByIdQuery : IRequest<ProductVariantGetByIdDto>
+    {
+        public int Id { get; set; }
+
+        public ProductVariantGetByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
