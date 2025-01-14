e95bfcd1c3939574e5da649ed168a298e1103ce4
Sila Kuscu <silaksc27@gmail.com>
2025-01-14 15:06:30 +0300

    [Create] Created ProductVariant Update Commands

---
+++ ModusoftCRM.Application/Features/ProductVariants/Commands/Update/ProductVariantUpdateCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Application.Features.ProductVariants.Commands.Update
+{
+    public class ProductVariantUpdateCommand : IRequest<Response<Guid>>
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public int? Stock { get; set; }
+        public Guid? ProductId { get; set; }
+    }
+}
---
+++ ModusoftCRM.Application/Features/ProductVariants/Commands/Update/ProductVariantUpdateCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace ModusoftCRM.Application.Features.ProductVariants.Commands.Update
+{
+    public class ProductVariantUpdateCommandValidator : AbstractValidator<ProductVariantUpdateCommand>
+    {
+        public ProductVariantUpdateCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Ürün varyant ID'si boş olamaz.");
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("İsim boş olamaz.")
+                .MaximumLength(100).WithMessage("İsim 100 karakterden uzun olamaz.");
+            RuleFor(x => x.Price)
+                .GreaterThanOrEqualTo(0).When(x => x.Price.HasValue)
+                .WithMessage("Fiyat negatif olamaz.");
+            RuleFor(x => x.Stock)
+                .GreaterThanOrEqualTo(0).When(x => x.Stock.HasValue)
+                .WithMessage("Stok negatif olamaz.");
+            RuleFor(x => x.ProductId)
+                .NotEmpty().WithMessage("Ürün ID'si boş olamaz.");
+        }
+    }
+}
