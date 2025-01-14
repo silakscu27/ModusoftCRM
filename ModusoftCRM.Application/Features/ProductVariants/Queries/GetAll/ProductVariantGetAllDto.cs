e22802c14e3bf7b2b6205b91c1d45e60867e02c3
Sila Kuscu <silaksc27@gmail.com>
2025-01-14 15:07:55 +0300

    [Create] Created ProductVariant Get All Queries

---
+++ ModusoftCRM.Application/Features/ProductVariants/Queries/GetAll/ProductVariantGetAllDto.cs
@@ -0,0 +1,17 @@
+﻿namespace ModusoftCRM.Application.Features.ProductVariants.Queries.GetAll
+{
+    public class ProductVariantGetAllDto
+    {
+        public int Id { get; set; }
+        public string ProductId { get; set; } = string.Empty;
+        public int Thickness { get; set; }
+        public double Width { get; set; }
+        public double Length { get; set; }
+        public decimal BasePrice { get; set; }
+        public string? StockCode { get; set; }
+        public int? BrandId { get; set; }
+        public string BrandName { get; set; } = string.Empty;
+        public int? ColourId { get; set; }
+        public string ColourName { get; set; } = string.Empty;
+    }
+}
