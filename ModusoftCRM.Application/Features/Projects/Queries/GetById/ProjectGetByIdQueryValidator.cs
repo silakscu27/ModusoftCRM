6cd868e7508db77d4c495a68dded725cd767fe7f
Sila Kuscu <silaksc27@gmail.com>
2025-01-15 15:19:51 +0300

    [Create] Created Project GetById Queries

---
+++ ModusoftCRM.Application/Features/Projects/Queries/GetById/ProjectGetByIdQueryValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using ModusoftCRM.Application.Common.Interfaces;
+
+namespace ModusoftCRM.Application.Features.Projects.Queries.GetById
+{
+    public class ProjectGetByIdQueryValidator : AbstractValidator<ProjectGetByIdQuery>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public ProjectGetByIdQueryValidator(ITenantService tenantService)
+        {
+            _context = tenantService.GetDbInstance(CancellationToken.None).GetAwaiter().GetResult();
+
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .WithMessage("Lütfen bir proje ID'si giriniz.");
+
+            RuleFor(x => x.Id)
+                .MustAsync(AnyProjectAsync)
+                .WithMessage("Seçili proje bulunamadı.");
+        }
+
+        private Task<bool> AnyProjectAsync(string id, CancellationToken cancellationToken)
+        {
+            return _context.Projects.AnyAsync(x => x.Id == id, cancellationToken);
+        }
+    }
+}
