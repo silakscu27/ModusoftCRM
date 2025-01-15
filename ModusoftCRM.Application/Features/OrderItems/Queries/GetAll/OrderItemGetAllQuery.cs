ff90fb4cac31d63822694a589361d8e033082f8e
Sila Kuscu <silaksc27@gmail.com>
2025-01-15 15:17:20 +0300

    [Create] Created OrderItem GetAll Queries

---
+++ ModusoftCRM.Application/Features/OrderItems/Queries/GetAll/OrderItemGetAllQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ModusoftCRM.Application.Features.OrderItems.Queries.GetAll;
+
+namespace ModusoftCRM.Application.Features.OrderItems.Queries
+{
+    public class OrderItemGetAllQuery : IRequest<List<OrderItemGetAllDto>>
+    {
+    }
+}
