22dc796b05cdf75afcacec3f8ca2db48cd5b88eb
Sila Kuscu <silaksc27@gmail.com>
2025-01-15 09:16:41 +0300

    [Create] Created OrderItem entity

---
+++ ModusoftCRM.Domain/Entities/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using ModusoftCRM.Domain.Common;
+using ModusoftCRM.Domain.Enums;
+
+namespace ModusoftCRM.Domain.Entities
+{
+    public class OrderItem : EntityBase<int>
+    {
+        public Guid OrderId { get; set; }
+        public Order Order { get; set; } = null!; 
+        public string ProductVariantId { get; set; } = null!;
+        public ProductVariant ProductVariant { get; set; } = null!;
+        public int Quantity { get; set; }
+        public DiscountType DiscountType { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal FinalUnitPrice { get; set; }
+    }
+}
