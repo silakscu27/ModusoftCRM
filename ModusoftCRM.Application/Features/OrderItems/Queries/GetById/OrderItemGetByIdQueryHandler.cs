5b7dbe516b81e6075ad2ee9b091f6c9633b50f1a
Sila Kuscu <silaksc27@gmail.com>
2025-01-15 15:17:35 +0300

    [Create] Created OrderItem GetById Queries

---
+++ ModusoftCRM.Application/Features/OrderItems/Queries/GetById/OrderItemGetByIdQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ModusoftCRM.Application.Common.Interfaces;
+
+namespace ModusoftCRM.Application.Features.OrderItems.Queries.GetById
+{
+    public class OrderItemGetByIdQueryHandler : IRequestHandler<OrderItemGetByIdQuery, OrderItemGetByIdDto>
+    {
+        private readonly ITenantService _tenantService;
+        private readonly IMapper _mapper;
+
+        public OrderItemGetByIdQueryHandler(ITenantService tenantService, IMapper mapper)
+        {
+            _tenantService = tenantService;
+            _mapper = mapper;
+        }
+
+        public async Task<OrderItemGetByIdDto> Handle(OrderItemGetByIdQuery request, CancellationToken cancellationToken)
+        {
+            var dbContext = await _tenantService.GetDbInstance(cancellationToken);
+
+            var orderItem = await dbContext.OrderItems
+                .AsNoTracking()
+                .Include(o => o.ProductVariant)  
+                .FirstOrDefaultAsync(x => x.Id == request.OrderItemId, cancellationToken);
+
+            if (orderItem == null)
+            {
+                throw new KeyNotFoundException("Sipariş öğesi bulunamadı.");
+            }
+
+            return _mapper.Map<OrderItemGetByIdDto>(orderItem);
+        }
+    }
+}
