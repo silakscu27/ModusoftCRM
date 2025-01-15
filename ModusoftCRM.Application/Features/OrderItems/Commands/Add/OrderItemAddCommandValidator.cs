415085e5d22748674c2c2ea1bbb3215608590b3c
Sila Kuscu <silaksc27@gmail.com>
2025-01-15 15:16:49 +0300

    [Create] Created OrderItem Add Commands

---
+++ ModusoftCRM.Application/Features/OrderItems/Commands/Add/OrderItemAddCommandValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using ModusoftCRM.Application.Features.OrderItems.Commands.Add;
+
+namespace ModusoftCRM.Application.Features.OrderItems.Commands
+{
+    public class OrderItemAddCommandValidator : AbstractValidator<OrderItemAddCommand>
+    {
+        public OrderItemAddCommandValidator()
+        {
+            RuleFor(x => x.OrderId)
+                .NotEmpty()
+                .WithMessage("Sipariş ID boş geçilemez.");
+
+            RuleFor(x => x.ProductVariantId)
+                .NotEmpty()
+                .WithMessage("Ürün varyantı ID boş geçilemez.");
+
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0)
+                .WithMessage("Miktar sıfırdan büyük olmalıdır.");
+
+            RuleFor(x => x.DiscountAmount)
+                .GreaterThanOrEqualTo(0)
+                .WithMessage("İndirim tutarı sıfır veya daha büyük olmalıdır.");
+
+            RuleFor(x => x.UnitPrice)
+                .GreaterThan(0)
+                .WithMessage("Birim fiyat sıfırdan büyük olmalıdır.");
+
+            RuleFor(x => x.FinalUnitPrice)
+                .GreaterThanOrEqualTo(x => x.UnitPrice)
+                .WithMessage("Nihai birim fiyat, birim fiyatından küçük olamaz.");
+        }
+    }
+}
