f0d46ce9a2368e36642063cd24b26a254a059f71
Sila Kuscu <silaksc27@gmail.com>
2025-01-15 15:19:40 +0300

    [Create] Created Project GetAll Queries

---
+++ ModusoftCRM.Application/Features/Projects/Queries/GetAll/ProjectGetAllDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ModusoftCRM.Application.Features.Projects.Queries.GetAll
+{
+    public class ProjectGetAllDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
