623051807d672ed6dee86645fc46d0c5b8c3ccac
Sila Kuscu <silaksc27@gmail.com>
2025-01-15 09:45:38 +0300

    [Create] Created ProposalItem entity

---
+++ ModusoftCRM.Domain/Entities/ProposalItem.cs
@@ -0,0 +1,23 @@
+﻿using ModusoftCRM.Domain.Common;
+using ModusoftCRM.Domain.Enums;
+
+namespace ModusoftCRM.Domain.Entities
+{
+    public class ProposalItem : EntityBase<Guid>
+    {
+        public Guid ProposalId { get; set; }
+        public Proposal Proposal { get; set; } = null!;
+
+        public string ProductVariantId { get; set; } = null!;
+        public ProductVariant ProductVariant { get; set; } = null!;
+
+        public int Quantity { get; set; }
+
+        public DiscountType DiscountType { get; set; }
+        public decimal DiscountAmount { get; set; }
+
+        public decimal UnitPrice { get; set; }
+        public decimal FinalUnitPrice { get; set; }
+
+    }
+}
