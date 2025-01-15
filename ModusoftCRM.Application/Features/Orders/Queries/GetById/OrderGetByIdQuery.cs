5e4c9b95addd5d69b9b11430cc4045fd6422c0d3
Sila Kuscu <silaksc27@gmail.com>
2025-01-15 15:18:30 +0300

    [Create] Created Order GetById Queries

---
+++ ModusoftCRM.Application/Features/Orders/Queries/GetById/OrderGetByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace ModusoftCRM.Application.Features.Orders.Queries.GetById
+{
+    public class OrderGetByIdQuery : IRequest<OrderGetByIdDto>
+    {
+        public Guid OrderId { get; set; }
+
+        public OrderGetByIdQuery(Guid orderId)
+        {
+            OrderId = orderId;
+        }
+    }
+}
---
+++ ModusoftCRM.Application/Features/Orders/Queries/GetById/OrderGetByIdQueryValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using ModusoftCRM.Application.Common.Interfaces;
+
+namespace ModusoftCRM.Application.Features.Orders.Queries.GetById
+{
+    public class OrderGetByIdQueryValidator : AbstractValidator<OrderGetByIdQuery>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public OrderGetByIdQueryValidator(ITenantService tenantService)
+        {
+            _context = tenantService.GetDbInstance(CancellationToken.None).GetAwaiter().GetResult();
+
+            RuleFor(x => x.OrderId)
+                .NotEmpty()
+                .WithMessage("Lütfen bir sipariş ID'si giriniz.");
+
+            RuleFor(x => x.OrderId)
+                .MustAsync(AnyOrderAsync)
+                .WithMessage("Seçili sipariş bulunamadı.");
+        }
+
+        private Task<bool> AnyOrderAsync(Guid orderId, CancellationToken cancellationToken)
+        {
+            return _context.Orders.AnyAsync(x => x.Id == orderId, cancellationToken);
+        }
+    }
+}
