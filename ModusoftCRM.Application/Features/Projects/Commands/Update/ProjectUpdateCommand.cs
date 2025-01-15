e208c33c277dabefc5a30b98e0f86b1c641eff83
Sila Kuscu <silaksc27@gmail.com>
2025-01-15 15:19:24 +0300

    [Create] Created Project Update Commands

---
+++ ModusoftCRM.Application/Features/Projects/Commands/Update/ProjectUpdateCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using ModusoftCRM.Domain.Common;
+
+namespace ModusoftCRM.Application.Features.Projects.Commands.Update
+{
+    public class ProjectUpdateCommand : IRequest<Response<string>>
+    {
+        public string Id { get; set; } = string.Empty;
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
